0d65981693cbfe21094ef2e841f5e239f58baf5d
Serhii Ternovtsov <tserg42@gmail.com>
2024-07-18 00:33:38 +0300

    implement addition

--- tests/mycalculatordapp.js
+++ tests/mycalculatordapp.js
@@ -1,13 +1,13 @@
 const assert = require('assert')
 const anchor = require('@coral-xyz/anchor')
-const {SystemProgram} = anchor.web3
+const { SystemProgram } = anchor.web3
 describe('mycalculatordapp', () => {
     const provider = anchor.getProvider()
     anchor.setProvider(provider)
     const calculator = anchor.web3.Keypair.generate()
     const program = anchor.workspace.Mycalculatordapp
 
-    it('Creates a calculator', async() => {
+    it('Creates a calculator', async () => {
         await program.rpc.create('Hello world', {
             accounts: {
                 calculator: calculator.publicKey,
@@ -18,6 +18,16 @@ describe('mycalculatordapp', () => {
         })
         const account = await program.account.calculator.fetch(calculator.publicKey)
         assert.ok(account.greeting == 'Hello world')
+    })
+
+    it('Adds 2 numbers', async () => {
+        await program.rpc.add(new anchor.BN(2), new anchor.BN(40), {
+            accounts: {
+                calculator: calculator.publicKey
+            }
+        })
+        const account = await program.account.calculator.fetch(calculator.publicKey)
+        assert.deepEqual(account.result, new anchor.BN(42))
     })
 
 })
